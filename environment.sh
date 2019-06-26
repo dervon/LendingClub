4a9490f243b851f75b655fee1995853c27349215
Dervon <dervon.he@gmail.com>
2019-06-26 13:58:02 +0000

    environment setting and download LoanStats

---
+++ environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt install python3-pip
+pip3 install tensorflow
+pip3 install numpy 
+pip3 install pandas 
+wget https://resources.lendingclub.com/LoanStats_2016Q3.csv.zip
+unzip LoanStats_2016Q3.csv.zip
